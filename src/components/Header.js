47f4ec48b6a0c63ed3932c2c3e60077573b79056
Trey Williams <kingproto3232@gmail.com>
2022-10-12 19:50:04 -0400

    Header complete! going to Sections

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <link rel="stylesheet" href="style.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <script src="main.js"></script>
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap"
+        rel="stylesheet"
+      />
+
+      <title>Trey Williams</title>
+    </div>
+  );
+}
+
+export default Header;
